d7fdcaa48cee0c4d908111e706bf95776d7d83dd
Álvaro García <maxpowel@gmail.com>
2018-03-15 14:21:13 +0100

    Added support for custom handlers #1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name='applauncher',
   packages=['applauncher'],
-  version='1.011',
+  version='1.02',
   description='App launcher and base environment',
   author='Alvaro Garcia Gomez',
   author_email='maxpowel@gmail.com',
