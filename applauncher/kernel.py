ae77712a2e5091dc08405c9a705c3a006c1b520d
Álvaro García <maxpowel@gmail.com>
2018-03-19 19:41:59 +0100

    Close issue #3

--- applauncher/kernel.py
+++ applauncher/kernel.py
@@ -4,23 +4,26 @@ import zope.event
 import mapped_config.loader
 from abc import ABCMeta, abstractmethod
 import six
+import signal
+import concurrent.futures
+from concurrent.futures._base import Future
 
 # This class is only used for an friendly injection of configuration
 class Configuration(object):
     pass
-
 
 class Environments(object):
     DEVELOPMENT = "dev"
     PRODUCTION = "prod"
     TEST = "test"
 
-
 class KernelReadyEvent(object):
     pass
 
-
 class InjectorReadyEvent(object):
+    pass
+
+class KernelShutdownEvent(object):
     pass
 
 
@@ -34,6 +37,9 @@ class Kernel(object):
         self.bundles = bundles
         self.environment = environment
         self.log_handlers = []
+        self.is_shutdown = False
+        self.thread_pool = concurrent.futures.ThreadPoolExecutor()
+        self.running_services = []
 
         try:
             self.configuration = self.load_configuration(environment)
@@ -62,8 +68,22 @@ class Kernel(object):
             raise e
 
         self.configure_logger(environment=environment)
+        self.register_signals()
         logging.info("Kernel Ready")
         zope.event.notify(KernelReadyEvent())
+
+    def register_signals(self):
+        signal.signal(signal.SIGINT, self._signal_handler)
+        signal.signal(signal.SIGTERM, self._signal_handler)
+
+    def _signal_handler(self, signal, frame):
+        self.shutdown()
+
+    def __enter__(self):
+        return self
+
+    def run_service(self, service_function, *args):
+        self.running_services.append(self.thread_pool.submit(service_function, *args))
 
     def configure_logger(self, environment):
         log_level = logging.INFO
@@ -76,7 +96,6 @@ class Kernel(object):
             self.log_handlers.append(ch)
             log_level = logging.INFO
 
-
         logging.basicConfig(level=log_level, handlers=self.log_handlers)
         logging.info("Logger ready")
 
@@ -86,16 +105,30 @@ class Kernel(object):
         config = c.load_config(self.configuration_file, self.parameters_file)
         try:
             config = c.build_config(config, mappings)
-        except mapped_config.loader.NoValueException as ex:
-            print("Configuration error: " + str(ex))
-            exit()
-        except mapped_config.loader.NodeIsNotConfiguredException as ex:
-            print("Configuration error: " + str(ex))
-            exit()
-        except mapped_config.loader.IgnoredFieldException as ex:
+        except (
+                mapped_config.loader.NoValueException,
+                mapped_config.loader.NodeIsNotConfiguredException,
+                mapped_config.loader.IgnoredFieldException
+        ) as ex:
             print("Configuration error: " + str(ex))
             exit()
 
         return config
 
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.shutdown()
+
+    def shutdown(self):
+        if not self.is_shutdown:
+            self.is_shutdown = True
+            logging.info("Kernel shutting down")
+            zope.event.notify(KernelShutdownEvent())
+            self.thread_pool.shutdown()
+            logging.info("Kernel shutdown")
+
+
+    def wait(self):
+        for i in self.running_services:
+            # Wait for all services to finish
+            i.result()
 
